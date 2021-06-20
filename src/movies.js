63392298c2a31ed203db8e410de2a3f575e3cba5
Florian Becker <becker_florian@online.de>
2021-06-20 21:56:01 +0200

    added the to dos for the iterations

--- src/movies.js
+++ src/movies.js
@@ -8,6 +8,11 @@ function getAllDirectors(movies) {
   return movieDirectors
 }
 
+/*
+to do for the bonus:
+-> .reduce() the duplicates of directors
+*/
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(movies) {
   if (movies.length === 0) {
@@ -19,6 +24,12 @@ function howManyMovies(movies) {
   return spielbergMovies;
 }
 
+/*
+to do:
+-> .filter() movies and go for director "Steven Spielberg"
+-> only want to have the drama movies of Steven Spielberg: filter for genre "Drama" in the array movies and then in the array of Genre (using .includes()?)
+*/
+
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(averageScore) {
   if (averageScore.length === 0) {
@@ -26,14 +37,44 @@ function scoresAverage(averageScore) {
   }
 }
 
+/*
+to do: 
+-> hint: .reduce() the data to a single value (as I only need the score forthis iteration, except the amount of = movies.length)
+-> sum or total of entire scores
+-> averageScore of movies = total score/amount of movies
+-> round score to 2 decimals = math.round(averageScore * 100 ) / 100
+*/
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(averageDramaScore) {}
+
+/*
+to do: 
+-> filter drama movies in new array => needs to return a num (newArray.length for the amount of drama movies)
+-> sum or total of drama movie scores
+-> averageScore of drama movies = total score/amount of drama movies
+-> round drama score to 2 decimals = math.round(averageScore * 100 ) / 100
+*/
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
 
+/*
+to do: 
+-> .sort() the movies in ascending order by their release year
+-> extra: two movies in the same year => order alphabetical (title)
+!!! do not mutate the original array - take care by using .sort()
+*/
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
+
+/*
+to do:
+-> .sort() the array movies alphabetical (going for title)
+-> .reduce() as only the title is demanded
+-> return the 20 first movies in the new array ordered alphabetically (slice method [0, 20])
+*/
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
